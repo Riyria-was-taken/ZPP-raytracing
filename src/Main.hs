b1ffd0083fb46663a71c207adcc8b234b356e42f
Jagoda Kamińska <jk406179@students.mimuw.edu.pl>
2023-09-08 15:50:02 +0200

    new scenes to render

--- src/Main.hs
+++ src/Main.hs
@@ -20,7 +20,22 @@ materialRight = Metal { albedo = Vec3 { x = 0.8, y = 0.6, z = 0.2 }, fuzz = 1.0 
 worlds :: [HittableList] = [[Sphere { center = Vec3 {x = 0, y = 0, z = -1 }, radius = 0.5, material = materialCenter },
                  Sphere { center = Vec3 {x = 0, y = -100.5, z = -1}, radius = 100, material = materialGround},
                  Sphere { center = Vec3 {x = -1, y = 0, z = -1}, radius = 0.5, material = materialLeft},
-                 Sphere { center = Vec3 {x = 1, y = 0, z = -1}, radius = 0.5, material = materialRight}]]
+                 Sphere { center = Vec3 {x = 1, y = 0, z = -1}, radius = 0.5, material = materialRight}],
+                 [Sphere { center = Vec3 {x = 0, y = -100.5, z = -1}, radius = 100, material = materialGround},
+                 Sphere { center = Vec3 {x = -2, y = 0, z = -2}, radius = 0.1, material = materialLeft},
+                 Sphere { center = Vec3 {x = -1, y = 3, z = -1}, radius = 0.3, material = materialRight},
+                 Sphere { center = Vec3 {x = -0.5, y = 0, z = -3}, radius = 0.5, material = materialCenter},
+                 Sphere { center = Vec3 {x = -3, y = 1, z = -4}, radius = 1, material = materialGround},
+                 Sphere { center = Vec3 {x = 0, y = 2, z = -1}, radius = 2, material = materialRight}],
+                [
+                 Sphere { center = Vec3 {x = 0, y = -100.5, z = -1}, radius = 100, material = materialGround},
+                 Sphere { center = Vec3 {x = -2, y = 0, z = -5}, radius = 0.1, material = materialLeft},
+                 Sphere { center = Vec3 {x = -1, y = 3, z = -20}, radius = 5, material = materialRight},
+                 Sphere { center = Vec3 {x = -0.5, y = 0, z = -4}, radius = 0.5, material = materialCenter},
+                 Sphere { center = Vec3 {x = -3, y = 1, z = -10}, radius = 1, material = materialGround},
+                 Sphere { center = Vec3 {x = -2, y = 0, z = -30}, radius = 4, material = materialLeft},
+                 Sphere { center = Vec3 {x = -1, y = 3, z = -3}, radius = 0.3, material = materialRight},
+                 Sphere { center = Vec3 {x = -4, y = -2, z = -1}, radius = 0.2, material = materialRight}]]
 
 
 main :: IO ()
